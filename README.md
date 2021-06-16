fa5585675968c615ec73ecb3c0f0bbf7fbc906fa
dili02 <dili02@gmail.com>
2021-06-16 16:35:31 -0300

    Login in frontend

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Full Stack Open 2021 - Notes App Web
 App proposed by the program [Full Stack Open](https://fullstackopen.com/en) by the **University of Helsinki**.
 
-## Completed Parts
-**Part 0** - Fundamentals of Web apps <br>
-**Part 1** - Introduction to React <br>
-**Part 2** - Communicating with server <br>
+## Part 2 Communicating with server
+we will take a look at how to render a data collection, like a list of names, to the screen. After this, we will inspect how a user can submit data to a React application using HTML forms. Next, our focus shifts towards looking at how JavaScript code in the browser can fetch and handle data stored in a remote backend server. Lastly, we will take a quick look at a few simple ways of adding CSS styles to our React applications.<br>
+**a** - Rendering a collection, modules <br>
+**b** - Forms <br>
+**c** - Getting data from server <br>
+**d** - Altering data in server <br>
+**e** - Adding styles to React App <br>
+
+## Part 5 Testing React app
+In this part we return to the frontend, first looking at different possibilities for testing the React code. We will also implement token based authentication which will enable users to log in to our application.<br>
+**a** - Login in frontend <br>
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import noteService from "./services/notes";
+import loginService from './services/login' 
 import Note from "./components/Note";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
@@ -9,12 +10,54 @@ function App() {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes);
     });
   }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogout = () => {
+    setUser(null)
+    noteService.setToken(user.token)
+    window.localStorage.removeItem('loggedNoteappUser')
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   const addNote = (event) => {
     event.preventDefault();
@@ -54,6 +97,40 @@ function App() {
       });
   };
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  )
+
   const notesToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true);
@@ -64,17 +141,56 @@ function App() {
 
       <Notification message={errorMessage} />
 
+      {
+        user === null
+          ? loginForm()
+          : <div>
+            <p>
+              {user.name} logged-in
+              <button onClick={handleLogout}>
+                logout
+              </button>
+            </p>
+            {noteForm()}
+          </div>
+      }
+
+      {/* {user === null && loginForm()}
+      {user !== null && noteForm()} */}
+
+      {/* <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form> */}
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
         </button>
       </div>
 
-      <form onSubmit={addNote}>
+      {/* <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
-
+ */}
       <ul>
         {notesToShow.map((note) => (
           <Note
